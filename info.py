9d13b19c30720c77e1ea0c5542dd8131bff303db
ISTAIL <israilsk14847@gmail.com>
2024-11-22 16:09:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ API_HASH = environ.get('API_HASH', 'db0da205c5ec06e9b9881e5fbb199dd7')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5495693025').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5495693025', '6735840265').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Skisrailz__100") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002459211482'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/nismoviesmovies')
